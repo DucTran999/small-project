a99123abef94a73019828b4443c92c6acb08154c
ducnta <tranaduc9x@gmail.com>
2023-01-18 21:54:18 +0700

    New class to validate all player input
    2 methods:
    validate_player_choice() func. Ensure player input is a number from 1 to 3.
    validate_player_name() func. Ensure player not empty and not over 20 characters.

---
+++ hippocampe_race_resource/validates.py
@@ -0,0 +1,30 @@
+class Validate:
+    """Validate all input from player."""
+    
+    def validate_player_choice(self):
+        """Ensure player input only 1, 2 or 3"""
+        
+        while True:
+            try:
+                player_inp = int(input("Your choice: "))
+                if player_inp > 0 and player_inp < 4:
+                    return player_inp
+                else:
+                    print("Please enter 1, 2 or 3!")
+            except ValueError:
+                print("Enter number only!")
+
+    def validate_player_name(self):
+        """Ensure player input name not over 20 chars and not empty.
+            Nickname accepted.
+        """
+        
+        while True:
+            player_inp = input("Enter your name: ").strip()
+            if len(player_inp) == 0:
+                print("Empty name or all whitespace is not excepted!")
+            elif len(player_inp) > 20:
+                print("Your name is over 20 chars!")
+            else:
+                return player_inp.capitalize()
+            
