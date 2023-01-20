280920daff2e48d66f02ab12f2a58816f6aa8ac4
Ductran <tranaduc9x@gmail.com>
2023-01-21 02:24:34 +0700

    fix error while printing race to console
    Two new function make programme access box objects easier

--- hippocampe_race_resource/boxes.py
+++ hippocampe_race_resource/boxes.py
@@ -7,8 +7,14 @@ class Box:
     def __str__(self) -> str:
         return f"pos: {self.position}, type: {self.type}"
     
+    def get_pos(self):
+        return self.position
+    
+    def get_type(self):
+        return self.type
+    
     def convert_type_to_signal(self, type_string):
         if type_string == 'mystery':
-            return '?'
+            return '_?_'
         elif type_string == 'danger':
-            return '!'
+            return '_!_'
