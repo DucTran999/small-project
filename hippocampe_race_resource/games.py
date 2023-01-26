862fb74b67cc7cd36cac7502a7475e6f2f2d6baf
Ductran <tranaduc9x@gmail.com>
2023-01-26 18:41:57 +0700

    Add class game:
    This class will hanle all process of this game.
    Some essential functions:
    Create player profile
    Create race for each player
    Hanle dice rolling and update hippocampuses info depend on dice event.
    Print result after each round and the final result when the winner is
    found.

--- hippocampe_race_resource/games.py
+++ hippocampe_race_resource/games.py
@@ -1,96 +1,351 @@
-from time import sleep, time
-from random import randint, seed
+import json
+
+from . players import Player
+from . races import Race
+from . dice_side import Dice
+from random import choice
 
 class Game:
     
     def __init__(self, hr_game):
         self.hr_game = hr_game
         self.validates = hr_game.validates
-        self.player1 = hr_game.player1
-        self.player2 = hr_game.player2
         self.game_screen = hr_game.screens
+        self.dice = Dice()
         
     def create_new_game(self, game_mode):
         if game_mode == '1 player':
-            self.start_single_game()
+            self.start_single_game(game_mode)
         elif game_mode == '2 players':
-            self.start_multi_game()
-        
-    def start_single_game(self):
-        """validate player input and create player profile."""
-        
-        player_name = self.validates.validate_player_name()
-        self.player1.set_name(player_name)
-        self.player2.set_name("Computer")
-        print(f"\t\t\t{self.player1.get_name()} vs {self.player2.get_name()}")
-        
-        self.create_map()
-        
-    def start_multi_game(self):
-        player_name1 = self.validates.validate_player_name()
-        player_name2 = self.validates.validate_player_name()
-        self.player1.set_name(player_name1)
-        self.player2.set_name(player_name2)
-        print(f"\t\t\t{self.player1.get_name()} vs {self.player2.get_name()}")
-        
+            self.start_multi_game(game_mode)
 
-    def create_map(self):
-        # delay between p1_box_pos and p2_box_pos to ensure they are diff.
-        p1_box_pos = self.create_box_in_race(self.player1.get_name())
-        self.load_map()
-        p2_box_pos = self.create_box_in_race(self.player2.get_name())
-        #classify box
-        p1_box_type = self.box_classify(p1_box_pos)
-        p2_box_type = self.box_classify(p2_box_pos)
-                
-        # Draw map
-        self.game_screen.draw_map(self.player1.get_name(),
-                                   self.player1.get_hippo(),
-                                   self.player2.get_name(),
-                                   self.player2.get_hippo(),
-                                   p1_box_type,
-                                   p2_box_type)
         
-    def load_map(self):
-        print("\tLoading map", end='')
-        for i in range(0, 5):
-            if i < 4:
-                print('.', end='')
-                sleep(0.6)
-            else:
-                print()
-                
-    def create_box_in_race(self, player_name):
-        """diffusion var use for pseudo random more likely random,
-            then we will have random number of box and their position and type.
-            type of box depend on the index oven for sp_gift and odd for danger.
-            Ex:  1  2  3  4
-            pos [3, 9, 4, 1] -> 9 and 1 are special gift, 3 and 4 are danger box 
+    def start_single_game(self, game_mode):
+        """ Processing flow:
+            1. create player profile
+            2. create race
+            3. Looping:
+                roll dice
+                update_hippo_list()
+                show round result
+        """
+        # Create player profile
+        players = self.create_player_profile(game_mode)
+        p1_name = players[0].get_name()
+        p2_name = players[1].get_name()
+        print(f"\t\t\t{p1_name} vs {p2_name}")
+        
+        # Create player races:
+        p1_race = Race(p1_name)
+        self.game_screen.show_deday_create_race(p1_name)
+        self.game_screen.show_player_process(p1_race)
+        p2_race = Race(p2_name)
+        self.game_screen.show_deday_create_race(p2_name)
+        self.game_screen.show_player_process(p2_race)
+        boxes_event = self.load_box_event()
+        
+        game_finished = False
+        surrender_flag = 0
+        rnd = 1 
+        while not game_finished:
+            # Player 1's turn
+
+            while players[0].get_dice_roll() > 0:
+                # Rolling dice:
+                dice_face = self.dice.roll_dice(p1_name)
+                if dice_face == "sur":
+                    print(f"\t\t{p1_name} SURRENDER!")
+                    surrender_flag = 1
+                    break
+                # Update hippo list
+                p1_hpl = players[0].get_hippo_list()
+                p1_boxes = p1_race.get_boxes()
+                hp1_update = self.hanle_dice_side(dice_face, p1_hpl)
+                if hp1_update:
+                    players[0].update_hippo_list(hp1_update, p1_boxes,
+                                             dice_face, boxes_event)
+                players[0].decrease_dice_roll()
+            
+            # Computer turn
+            if surrender_flag == 1:
+                ptext = f"FINAL RESULT"
+                print(f"#{ptext:-^74}#")    
+                print(f"\t\t<_______________ The {p2_name} WIN_________________>")
+                print("-" * 78)
+                break
+
+            while players[1].get_dice_roll() > 0:
+                # Rolling dice:
+                dice_face = self.dice.roll_dice(p2_name, 1)
+                # Update hippo list
+                p2_hpl = players[1].get_hippo_list()
+                p2_boxes = p2_race.get_boxes()
+                hp2_update = self.hanle_dice_side(dice_face, p2_hpl, 1)
+                if hp2_update:
+                    players[1].update_hippo_list(hp2_update, p2_boxes,
+                                             dice_face, boxes_event)
+                players[1].decrease_dice_roll()
+            
+            #show round result
+            self.draw_map(p1_race, p2_race, players[0], players[1], rnd)
+            
+            # update p1 & p2 turn
+            check_p1 = players[0].is_valid_player()
+            check_p2 = players[1].is_valid_player()
+            if check_p1:
+                players[0].increase_dice_roll()
+            if check_p2:
+                players[1].increase_dice_roll()
+            if not check_p1 and not check_p2:
+                ptext = f"FINAL RESULT"
+                print(f"#{ptext:-^74}#")
+                p1_finished = players[0].count_finished_hippo()
+                p2_finished = players[1].count_finished_hippo()
+                if p1_finished == p2_finished:
+                    print("\t\t_____________DRAW GAME______________")
+                elif p1_finished > p2_finished:
+                    print(f"\t\t<____________ P1: {p1_name} WIN ___________>")
+                else:
+                    print(f"\t\t<____________ P2: {p2_name} WIN ___________>")
+                print("-" * 80)
+                game_finished = True
+            rnd += 1
+    
+    
+    def start_multi_game(self, game_mode):
+        """ Processing flow:
+            1. create player profile
+            2. create race
+            3. Looping:
+                roll dice
+                update_hippo_list()
+                show round result
         """
         
-        diffusion_var = int(time()) + len(player_name)
-        seed(diffusion_var + int(time()) + 1999)
-        number_of_box = randint(1, 4)
-
-        i = 1
-        box_position = []
-        seed(int(time()) + 2003)
-        while i <= number_of_box:
-            new_pos = randint(1, 12)
-            if new_pos not in box_position:
-                box_position.append(new_pos)
-                i += 1
-                
-        return box_position
-    
-    def box_classify(self, box_pos):
-        special_gift = []
-        danger_box = []
-        for i, value in enumerate(box_pos):
-            if i % 2 == 0:
-                special_gift.append(value)
-            else:
-                danger_box.append(value)
-        return [special_gift, danger_box]
+        # Create player profile
+        players = self.create_player_profile(game_mode)
+        p1_name = players[0].get_name()
+        p2_name = players[1].get_name()
+        print(f"\t\t\t{p1_name} vs {p2_name}")
         
+        # Create player races:
+        p1_race = Race(p1_name)
+        self.game_screen.show_deday_create_race(p1_name)
+        self.game_screen.show_player_process(p1_race)
+        p2_race = Race(p2_name)
+        self.game_screen.show_deday_create_race(p2_name)
+        self.game_screen.show_player_process(p2_race)
+        boxes_event = self.load_box_event()
+        
+        game_finished = False
+        surrender_flag = 0
+        # surrender_flag: p1: 1, p2 = 2
+        rnd = 1 
+        while not game_finished:
+            # Player 1's turn
+            while players[0].get_dice_roll() > 0:
+                # Rolling dice:
+                dice_face = self.dice.roll_dice(p1_name)
+                if dice_face == "sur":
+                    print(f"\t\t{p1_name} SURRENDER!")
+                    surrender_flag = 1
+                    break
+                # Update hippo list
+                p1_hpl = players[0].get_hippo_list()
+                p1_boxes = p1_race.get_boxes()
+                hp1_update = self.hanle_dice_side(dice_face, p1_hpl)
+                if hp1_update:
+                    players[0].update_hippo_list(hp1_update, p1_boxes,
+                                             dice_face, boxes_event)
+                players[0].decrease_dice_roll()
+            
+            #Player 2's turn
+            if surrender_flag == 1:
+                ptext = f"FINAL RESULT"
+                print(f"#{ptext:-^74}#")    
+                print(f"\t\t<_______________ P2: {p2_name} WIN _________________>")
+                print("-" * 78)
+                break
+            
+            while players[1].get_dice_roll() > 0:
+                # Rolling dice:
+                dice_face = self.dice.roll_dice(p2_name)
+                if dice_face == "sur":
+                    print(f"\t\t{p2_name} SURRENDER!")
+                    surrender_flag = 2
+                    break
+                # Update hippo list
+                p2_hpl = players[1].get_hippo_list()
+                p2_boxes = p2_race.get_boxes()
+                hp2_update = self.hanle_dice_side(dice_face, p2_hpl)
+                if hp2_update:
+                    players[1].update_hippo_list(hp2_update, p2_boxes,
+                                             dice_face, boxes_event)
+                players[1].decrease_dice_roll()
+            
+            if surrender_flag == 2:
+                ptext = f"FINAL RESULT"
+                print(f"#{ptext:-^74}#")    
+                print(f"\t\t<_______________ P1: {p1_name} WIN _________________>")
+                print("-" * 78)
+                break
+            
+            #show round result
+            self.draw_map(p1_race, p2_race, players[0], players[1], rnd)
+            
+            # update p1 & p2 turn
+            check_p1 = players[0].is_valid_player()
+            check_p2 = players[1].is_valid_player()
+            if check_p1:
+                players[0].increase_dice_roll()
+            if check_p2:
+                players[1].increase_dice_roll()
+            if not check_p1 and not check_p2:
+                self.game_screen.show_deday_result()
+                ptext = f"FINAL RESULT"
+                print(f"#{ptext:-^74}#")
+                p1_finished = players[0].count_finished_hippo()
+                p2_finished = players[1].count_finished_hippo()
+                if p1_finished == p2_finished:
+                    print("\t\t_____________DRAW GAME______________")
+                elif p1_finished > p2_finished:
+                    print(f"\t\t<____________{p1_name} WIN___________>")
+                else:
+                    print(f"\t\t<____________{p2_name} WIN___________>")
+                print("-" * 80)
+                game_finished = True
+            rnd += 1
+
+    def load_box_event(self):
+        file_path = "hippocampe_race_resource/box_event.json"
+        try:
+            with open(file_path, 'r') as file_obj:
+                contents = json.load(file_obj)
+        except Exception as e:
+            print("<!> Some problems was occured when loading box_event file")
+            print(f"<!> Error_name: {e}")
+        else:
+            boxes = self.classify_box_event(contents)
+            return boxes
+
+
+    def classify_box_event(self, boxes):
+        danger_boxes = []
+        mystery_boxes = []
+        
+        for box in boxes:
+            for box_key, box_value in box.items():
+                if box_key == "mystery":
+                    mystery_boxes.append(box)
+                    break
+                else:
+                    danger_boxes.append(box)
+                    break
+        return [mystery_boxes, danger_boxes]
+
+
+    def create_player_profile(self, game_mode):
+        if game_mode == '1 player':
+            """validate player input and create player profile.""" 
+            p1_name = self.validates.validate_player_name()
+            p1 = Player(p1_name.title())
+            p2 = Player("Computer")
+            return [p1, p2]
+        elif game_mode == '2 players':
+            print("P1", end=' ')
+            p1_name = self.validates.validate_player_name()
+            print("P2", end=' ')
+            p2_name = self.validates.validate_player_name()
+            p1 = Player(p1_name.title())
+            p2 = Player(p2_name.title())
+            return [p1, p2]
+
+    
+    def hanle_dice_side(self, dice_side, hpl, computer_flag=0):
+        """Determine which hippocampus will be update."""
+        if dice_side == 6:
+            hippo_chosen = self.get_dice_6_event(hpl, computer_flag)
+            return hippo_chosen
+        else:
+            hippo_chosen = self.get_other_dice_side_event(hpl, dice_side, 
+                                                          computer_flag)
+            return hippo_chosen
+
+    
+    def get_dice_6_event(self, hpl, computer_flag):
+        hpl_valid = self.hippo_valid_filter(hpl, 1)
+        if computer_flag == 1:
+            return choice(hpl_valid)
+        else:    
+            msg = "<?> Dice 6 allow move 1 hippo to start or move 6 steps!\n"
+            msg += "-> Enter hippo order (1, 2, etc.): "
+            while True:
+                try: 
+                    p_choice = int(input(msg))
+                    if p_choice in hpl_valid:
+                        return p_choice
+                    else:
+                        print(f"<!> Hippo {p_choice} is not valid!")
+                except ValueError:
+                    print("<!> Enter number only!")
+
+
+    def get_other_dice_side_event(self, hpl, dice_side, computer_flag):
+        hpl_valid = self.hippo_valid_filter(hpl, 0)
+        if computer_flag == 1:
+            if len(hpl_valid):
+                return choice(hpl_valid)
+            else:
+                print("I don't have hippo ready for the race!")
+                return None
+        else:
+            if len(hpl_valid):
+                msg = f"<?> Hippo can move {dice_side} steps.\n"
+                msg += "-> Enter hippo order (1, 2, etc.): " 
+                while True:
+                    try:
+                        p_choice = int(input(msg))
+                    except ValueError:
+                        print("Enter number only!")
+                    else:
+                        if p_choice in hpl_valid:
+                            return p_choice
+                        else:
+                            print(f"Hippo {p_choice} is not valid!")
+            else:
+                print("You don't have hippo ready for the race!")
+                return None
+
+
+    def hippo_valid_filter(self, hpl, event_6_mark):
+        """Return list hippo are conditioned
+            event_6_mark: is variable I use for separating event 6 and others.
+            So this function can call by all event dice.
+        """
+
+        hippo_valid = []
+        print("<?> Hippo valid:")
+        for hp in hpl:
+            if event_6_mark == 1:
+                if hp.get_state_code() == -1:
+                    print(f"\t<i> Hippo {hp.get_order()} is warming up!")
+                    hippo_valid.append(hp.get_order())
+            if hp.get_state_code() == 0:
+                print(f"\t<i> Hippo {hp.get_order()} is ready at the start!")
+                hippo_valid.append(hp.get_order())
+            elif hp.get_state_code() == 4:
+                print(f"\t<i> Hippo {hp.get_order()} is at steps {hp.get_position()}!")
+                hippo_valid.append(hp.get_order())
+        return hippo_valid
+
+    def draw_map(self, p1_race, p2_race, p1, p2, round):
+        text = f"ROUND {round}"
+        print(f"#{text:-^74}#")
+        self.game_screen.show_player_process(p1_race)
+        p1.show_hippo_list()
+        print(f".{'_' * 74}.")
+        self.game_screen.show_player_process(p2_race)
+        p2.show_hippo_list()
+        print(f"#{'-' * 74}#")
+
     
