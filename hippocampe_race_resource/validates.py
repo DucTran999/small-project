09b39c6bc96cde014eb3d9ab19b1456a68e26bf9
Ductran <tranaduc9x@gmail.com>
2023-01-26 18:22:25 +0700

    add direction mark in every messages
    Because the game print alot of infomation to the console screen.
    To make it familier to player I add some direction mark for this:
    <!> warning mark: when an error occured.
    <?> direction mark: question or requirement infomations need player input.
    -> input mark: where player need to input.
    <i> infomation mark: simply give infomations of hippocampuses.

--- hippocampe_race_resource/validates.py
+++ hippocampe_race_resource/validates.py
@@ -6,13 +6,13 @@ class Validate:
         
         while True:
             try:
-                player_inp = int(input("Your choice: "))
+                player_inp = int(input("-> Your choice: "))
                 if player_inp > 0 and player_inp < 4:
                     return player_inp
                 else:
-                    print("Please enter 1, 2 or 3!")
+                    print("<!> Please enter 1, 2 or 3!")
             except ValueError:
-                print("Enter number only!")
+                print("<!> Enter number only!")
 
     def validate_player_name(self):
         """Ensure player input name not over 20 chars and not empty.
@@ -22,19 +22,19 @@ class Validate:
         while True:
             player_inp = input("Enter your name: ").strip()
             if len(player_inp) == 0:
-                print("Empty name or all whitespace is not excepted!")
+                print("<!> Empty name or all whitespace is not excepted!")
             elif len(player_inp) > 20:
-                print("Your name is over 20 chars!")
+                print("<!> Your name is over 20 chars!")
             else:
                 return player_inp.capitalize()
 
     def validate_hippo_exist(self, order_list):
         while True:
             try:
-                player_inp = int(input("Enter hippo order: "))
+                player_inp = int(input("-> Enter hippo order: "))
                 if player_inp in order_list:
                     return player_inp
                 else:
-                    print("hippo order not valid!")
+                    print("<!> Hippo order not valid!")
             except ValueError:
-                print("Enter number only!")           
+                print("<!> Enter number only!")           
