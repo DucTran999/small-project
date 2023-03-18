f02f8254f8ac74f3d36774a83ead7a32706f3825
Daniel <tedomi999@gmail.com>
2023-03-18 18:17:04 +0700

    refactor: add new state named 'player_type'. For reducing duplicate code.
    Now, players are classified into two types: auto and manual. The game
    process applies for both types the same. The only thing is the "auto"
    player will automatically fill answer and the manual player must enter
    it. So we can reuse the code of the processing part instead of having to
    write code for both objects.

--- Seahorses Race/src/main/player/player.py
+++ Seahorses Race/src/main/player/player.py
@@ -5,20 +5,24 @@ class Player:
     """This class represent a player in game"""
     def __init__(self, name: str, 
                  seahorses: list[Seahorse],
+                 player_type: str,
                  turn_remain: int = 1,
                  turns_total: int = 0
                  ) -> None:
         """Player constructor
         
         Param:
-        - name: player name
+        - name: player name. default: computer
         - seahorses: player's seahorses
+        - player_type: default manual. Other type is auto use for creating computer
         - turn_remain: number of dice roll. This attribute will be affected by box
         event.
         - turn_total: total turn players used for moving all seahorses to finish.
+        player.
         """
         self.__name = name
         self.__seahorses = seahorses
+        self.__player_type = player_type
         self.__turn_remain = turn_remain
         self.__turns_total = turns_total
     
@@ -33,6 +37,14 @@ class Player:
     def seahorses(self) -> list[Seahorse]:
         return self.__seahorses
     
+    @property
+    def player_type(self):
+        return self.__player_type
+    
+    @player_type.setter
+    def player_type(self, new_type: str):
+        self.__player_type = new_type
+   
     @property
     def turn_remain(self) -> int:
         return self.__turn_remain
--- Seahorses Race/src/main/player/player_builder.py
+++ Seahorses Race/src/main/player/player_builder.py
@@ -4,6 +4,6 @@ from main.seahorse.seahorse import Seahorse
 
 class PlayerBuilder:
     
-    def create_player(self, name: str) -> Player:
+    def create_player(self, name: str, player_type: str = "manual") -> Player:
         seahorses = [Seahorse(seahorse_id) for seahorse_id in range(1, 4)]
-        return Player(name, seahorses)
+        return Player(name, seahorses, player_type)
